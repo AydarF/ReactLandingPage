83611d18b4a6e666ca8ee420f490e85f4e06d866
Aydar Fayzullin <aydar_fayzullin@yahoo.com>
2018-02-08 00:16:06 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Container, Row, Col } from 'react-grid-system';
+
+class App extends React.Component {
+  render() {
+    return (
+      <LandingPage />  
+    )
+  }
+}
+
+ReactDOM.render(
+  <App/>, document.getElementById('app')
+);
