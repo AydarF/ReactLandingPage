7be609b97be59cbffa1b843ff0209a4009c07c32
Aydar Fayzullin <aydar_fayzullin@yahoo.com>
2018-02-28 17:16:41 -0500

    Fix Navigation alignment

--- components/villages/neighborhoods/blocks/Navigation.js
+++ components/villages/neighborhoods/blocks/Navigation.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Component, Row, Col } from 'react-grid-system';
+import { Container, Row, Col } from 'react-grid-system';
 import Branding from './houses/Branding.js';
 import DemoLink from './houses/Demo Link.js';
 
 class Navigation extends React.Component {
   render() {
     return(
-      <div>
-        <Col lg={6}>
+      <Container>
+        <Row>
           <Branding />
-        </Col>
-        <Col lg={6}>
+        
           <DemoLink />
-        </Col>
-      </div>
+        </Row>
+      </Container>
     )
   }
 }
--- components/villages/neighborhoods/blocks/houses/Branding.js
+++ components/villages/neighborhoods/blocks/houses/Branding.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 class Branding extends React.Component {
   render() {
     return(
-      <div>
+      <div style={{paddingRight: '400px'}}>
         <Link to="/">
           <img className="branding" src="./images/Branding.svg"/>
         </Link>
