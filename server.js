cad041945fb8497e13b2b6eb619342395002495d
Aydar Fayzullin <aydar_fayzullin@yahoo.com>
2018-03-03 17:33:36 -0500

    Update server

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
+
 const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
+
+//Create our app
+
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(process.env.PORT || 5000, function() {
-  console.log("Express server listening on port %id in %s mode", this.address().port, app.settings.env);
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
