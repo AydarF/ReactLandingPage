fe728094aea9fa977c432f24b25053519a336bbb
Aydar Fayzullin <aydar_fayzullin@yahoo.com>
2018-03-07 15:25:02 -0500

    Configure Heroku deployment

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
-const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+var path = require('path');
+var express = require('express');
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+var app = express();
 
-app.listen(process.env.PORT || 5000, function() {
-  console.log("Express server listening on port %id in %s mode", this.address().port, app.settings.env);
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 8080);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
 });
